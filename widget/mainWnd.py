b9a674d7c5791fcacc5a818176d9e0fda90e937d
DevGuruPro <EricMoller4320@outlook.com>
2024-11-12 05:04:03 +0900

    Set color of status text.

--- widget/mainWnd.py
+++ widget/mainWnd.py
@@ -236,8 +236,18 @@ class MainWnd(QMainWindow):
 
     def monitor_gps_status(self, status):
         if status:
+            self.ui.gps_connection_status.setStyleSheet("""
+                padding: 5px;
+                border: 1px solid black;
+                color: green;
+                """)
             self.ui.gps_connection_status.setText("Connected")
         else:
+            self.ui.gps_connection_status.setStyleSheet("""
+                            padding: 5px;
+                            border: 1px solid black;
+                            color: red;
+                            """)
             self.ui.gps_connection_status.setText("Disconnected")
         if self.notify_gps:
             self.notify_thread = threading.Thread(target=self.beep_sound)
@@ -245,8 +255,18 @@ class MainWnd(QMainWindow):
 
     def monitor_rfid_status(self, status):
         if status == 1:
+            self.ui.rfid_connection_status.setStyleSheet("""
+                                        padding: 5px;
+                                        border: 1px solid black;
+                                        color: green;
+                                        """)
             self.ui.rfid_connection_status.setText("Connected")
         elif status == 2:
+            self.ui.rfid_connection_status.setStyleSheet("""
+                                        padding: 5px;
+                                        border: 1px solid black;
+                                        color: red;
+                                        """)
             self.ui.rfid_connection_status.setText("Disconnected")
         elif status == 3:
             tag = self.rfid.tag_data[0]
