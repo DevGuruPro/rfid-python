a962fda7fb2e4eeb483dc591410c24951a9d2d0e
DevGuruPro <EricMoller4320@outlook.com>
2024-12-02 06:25:17 +0100

    Update uninstall package.

--- script/uninstall_package.sh
+++ script/uninstall_package.sh
@@ -1,5 +1,34 @@
 #!/bin/bash  
 
+# Variables  
+APP_NAME="RFIDInventory"  # Name of your application  
+DESKTOP_FILE_NAME="${APP_NAME}.desktop"  # Name for the .desktop file  
+
+# Define the location of the .desktop file in the applications directory  
+desktop_applications_path="$HOME/.local/share/applications/$DESKTOP_FILE_NAME"  
+
+# Define the location of the .desktop file in the autostart directory  
+desktop_autostart_path="$HOME/.config/autostart/$DESKTOP_FILE_NAME"  
+
+# Remove the .desktop file for applications  
+if [ -f "$desktop_applications_path" ]; then  
+    rm "$desktop_applications_path"  
+    echo "Removed application list entry: $desktop_applications_path"  
+else  
+    echo "No application list entry found at: $desktop_applications_path"  
+fi  
+
+# Remove the .desktop file for autostart  
+if [ -f "$desktop_autostart_path" ]; then  
+    rm "$desktop_autostart_path"  
+    echo "Removed startup entry: $desktop_autostart_path"  
+else  
+    echo "No startup entry found at: $desktop_autostart_path"  
+fi
+
+# Inform the user that they might need to log out and log back in to see changes  
+echo "The application and startup entries have been removed."
+
 # Define package variables
 PACKAGE_NAME="RFIDInventory"
 SYSTEM_USER="rfidinv"
